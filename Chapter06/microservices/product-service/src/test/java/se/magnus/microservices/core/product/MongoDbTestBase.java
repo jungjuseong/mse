41d10bf6efaaa90a1d6aa8e1bde9b416dad4d575
jung juseong <jungjuseong@gmail.com>
2022-02-17 17:33:43 +0900

    11

--- Chapter06/microservices/product-service/src/test/java/se/magnus/microservices/core/product/MongoDbTestBase.java
+++ Chapter06/microservices/product-service/src/test/java/se/magnus/microservices/core/product/MongoDbTestBase.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 
 public abstract class MongoDbTestBase {
-  private static MongoDBContainer database = new MongoDBContainer("mongo:4.0.10");
+  private static MongoDBContainer database = new MongoDBContainer("mongo:4.4");
   
   static {
     database.start();
